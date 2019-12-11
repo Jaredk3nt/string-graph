43ebcec19b1b10cf65933ded97b57eddf255e255
Jared Jones <Jared.Jones@cdk.com>
2019-12-11 13:27:35 -0600

    Working on x-axis labels, fixing bug with uneven number causing x-axis labels to disappear

--- example.js
+++ example.js
@@ -1,6 +1,12 @@
-const { barGraph } = require('./lib/');
+const { barGraph } = require('./lib');
+const test = [
+  { label: 'first', data: 10 },
+  { label: 'second', data: 18 },
+  { label: 'third', data: 12 },
+  { label: 'fourth', data: 4 },
+  { label: 'fifth', data: 8 },
+];
 
-const testBarGraphData = [5, 10 , 15];
 console.log(
-  barGraph(testBarGraphData, { yLabels: true, yLabelCount: 3 })
-);
+  barGraph(test, { xLabelWidth: 6, columnWidth: 6, yLabels: true })
+)
